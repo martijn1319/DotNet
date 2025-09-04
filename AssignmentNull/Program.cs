7c16c7558b6ab74184c6c5b3e8c18ba0f230a77e
martijn1319 <martijn1319@gmail.com>
2025-09-04 15:49:00 +0200

    Initialized project

---
+++ AssignmentNull/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string input = Console.ReadLine();
+
+        // Split on spaces, remove empty entries
+        string[] words = input.Split(
+            new char[] { ' ', '\t' }, 
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        Console.WriteLine($"You entered {words.Length} words.");
+    }
+}
